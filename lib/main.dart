9a0100945b435ef674ddc3c3757814b15f0d78f4
JunchangZhao <jack19910603@163.com>
2019-04-15 17:17:03 +0800

    tweak drawer UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return OKToast(
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Note',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
--- lib/widget/home_drawer.dart
+++ lib/widget/home_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeDrawer extends StatelessWidget {
   @override
@@ -7,16 +8,84 @@ class HomeDrawer extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-
+          Container(
+            color: Colors.lightBlue,
+            height: 160,
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(28.0),
+                  child: SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: SvgPicture.asset(
+                      "icons/notebook.svg",
+                    ),
+                  ),
+                ),
+                Text(
+                  "Fltter Note",
+                  style: TextStyle(color: Colors.white, fontSize: 28),
+                ),
+              ],
+            ),
+          ),
           ListTile(
             leading: Icon(
               Icons.delete,
               color: Colors.blue,
             ),
-            title: Text("Go to Trash"),
+            title: Text(
+              "Go to Trash",
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
             trailing: Icon(
               Icons.chevron_right,
               color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            title: Text(
+              "Settings",
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
+            trailing: Icon(
+              Icons.chevron_right,
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: ListTile(
+                    leading: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: FittedBox(
+                        fit: BoxFit.fitHeight,
+                        child: Icon(
+                          Icons.account_circle,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      "13218019903",
+                      style: TextStyle(fontSize: 18, color: Colors.black54),
+                    ),
+                  ),
+                ),
+              ],
             ),
           )
         ],
