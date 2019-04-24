a8467c78a37f2dedf919a4184a41acfab10f80c3
jack_zhao <jack_zhao@trendmicro.com.cn>
2019-04-24 13:01:25 +0800

    tweak code structure

--- lib/view/splash_page.dart
+++ lib/view/splash_page.dart
@@ -2,28 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/common/common_datas.dart';
 import 'package:flutter_app/model/note_model.dart';
 import 'package:flutter_app/utils/sputils.dart';
+import 'package:flutter_app/viewmodel/impl/splash_vm_impl.dart';
+import 'package:flutter_app/viewmodel/splash_vm.dart';
 import 'package:flutter_svg/svg.dart';
 
 class _SplashPageState extends State<SplashPage> {
-  NoteModel _notePresenter = NoteModel();
+  SplashViewModel splashViewModel;
 
   @override
   void initState() {
-    SPKeys.ACCOUNT_NAME.getString().then((value) {
-      if (value == null || value.isEmpty) {
-        Navigator.of(context).pushReplacementNamed('/LoginPage');
-      } else {
-        initDatas().then((value) {
-          Navigator.of(context).pushReplacementNamed('/MainPage');
-        });
-      }
-    });
+    splashViewModel = SplashViewModelImpl(context);
+    splashViewModel.gotoNextPage();
     super.initState();
-  }
-
-  Future initDatas() async {
-    homePageNoteList = await _notePresenter.getAllNotes(context, false);
-    jwt = await SPKeys.JWT.getString();
   }
 
   @override
